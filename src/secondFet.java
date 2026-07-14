849613216b5f8170782021e35d050684dfcfc147
PraWin1045 <praveenprasad1045@gmail.com>
2026-07-14 21:43:46 +0530

    amuku dhumuku commit

--- src/secondFet.java
+++ src/secondFet.java
@@ -1,6 +1,7 @@
 public class secondFet {
 
     public static void main(String[] args) {
-        System.out.println("papumaaaaaaaaaaa");
+        System.out.println("papumaaaaaaaaaaa hello");
+        System.out.println("amuku dhumuku");
     }
 }
